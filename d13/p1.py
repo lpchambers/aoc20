e74c3910d223fe6cf707f87ca1dbbe00cbb54958
Lewis Chambers <lchambers@barrukka.au.boeing.com>
2020-12-13 15:59:04 +1000

    Formatting

--- d13/p1.py
+++ d13/p1.py
@@ -17,13 +17,9 @@ print(minbus * times[minbus])
 # P2
 buses = "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,421,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,19,x,x,x,x,x,x,x,x,x,29,x,487,x,x,x,x,x,x,x,x,x,x,x,x,13".split(',')
 # All numbers are prime
-
 #buses = "7,13,x,x,59,x,31,19".split(',')
-
-add_to = {int(bus): add for add, bus in enumerate(buses) if bus != 'x'}
-print(add_to)
-
 # Find t, s.t. t % bus == add_to[t]
+add_to = {int(bus): add for add, bus in enumerate(buses) if bus != 'x'}
 
 t = 0
 step = 1
